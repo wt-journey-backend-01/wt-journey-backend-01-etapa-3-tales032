62bd91359147fcd42cf99ae30f07a250e3955165
Tales de Oliveira <tales.oliveira032@gmail.com>
2025-08-09 20:40:28 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,10 @@
-const config = require("../knexfile");
+const knexConfig = require(".../knexfile");
 const knex = require("knex");
-require('dotenv').config();
 
-const db = knex(config.development);
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const config = knexConfig[nodeEnv];
+
+const db = knex(config);
 
 module.exports = db;
